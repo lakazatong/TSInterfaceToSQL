53b6e50fe80b75df6be914e8d14bf97e342b72c5
lakazatong <Bo_wood@outlook.fr>
2025-04-30 23:57:33 +0200

    WIP

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const fs = require("fs");
 const path = require("path");
 const { execSync } = require("child_process");
 
-const THRESHOLD = 3;
+const THRESHOLD = 5;
 
 // function objectDeepness(obj) {
 // 	if (typeof obj !== "object" || obj === null) return 0;
@@ -21,48 +21,130 @@ const THRESHOLD = 3;
 // 	return max + 1;
 // }
 
-function help(schema) {
-	let nullable = false;
-	let deepness = 0;
+function preprocess(schema) {
+	if (schema.type) {
+		if (Array.isArray(schema.type)) {
+			if (schema.type.length !== 2 || !schema.type.includes("null")) {
+				console.error("Impossible case reached", schema.type);
+				return;
+			}
+			schema.type = schema.type.filter((type) => type !== "null")[0];
+			schema.nullable = true;
+		}
 
-	function objectCase(schema) {
-		deepness =
-			Math.max(
-				...Object.values(schema.properties).map((prop) => {
-					help(prop);
-					return prop.deepness;
-				})
-			) + 1;
-	}
-
-	if (Array.isArray(schema.type)) {
-		if (schema.type.length !== 2 || !schema.type.includes("null")) {
-			console.error("Impossible case reached", schema.type);
+		if (schema.type === "object") {
+			schema.deepness =
+				1 +
+				Math.max(
+					...Object.entries(schema.properties).map(([k, v]) => {
+						preprocess(v);
+						v.nullable ??= !(schema.required ?? []).includes(k);
+						// console.log(k, v);
+						return v.deepness;
+					})
+				);
+			schema.table = Object.keys(schema.properties).length > THRESHOLD;
+			if (Object.values(schema.properties).every((s) => s.nullable)) {
+				schema.nullable = true;
+			}
+			if (Object.values(schema.properties).every((s) => s.table)) {
+				schema.table = true;
+			}
+		} else if (schema.type === "array") {
+			preprocess(schema.items);
+			schema.deepness = 1 + schema.items.deepness;
+			schema.table = true;
+		} else if (schema.type === "string") {
+			if (schema.format === "date-time") {
+				schema.type = "integer";
+				delete schema.format;
+			}
+			schema.deepness = 0;
+			schema.table = false;
+		} else {
+			schema.deepness = 0;
+			schema.table = false;
+		}
+	} else if (
+		Array.isArray(schema.anyOf) &&
+		schema.anyOf.length === 2 &&
+		schema.anyOf.some((s) => s && s.type === "null" && Object.keys(s).length === 1)
+	) {
+		const nonNullRaw = schema.anyOf.find((s) => !(s.type === "null" && Object.keys(s).length === 1));
+		if (!nonNullRaw) {
+			console.error("Impossible case reached", schema.anyOf);
 			return;
 		}
-		nullable = true;
-		if (schema.type.includes("object")) {
-			objectCase(schema);
-		}
-		schema.type = schema.type.filter((type) => type !== "null")[0];
-	} else if (schema.type === "object") {
-		objectCase(schema);
+		const nonNull = JSON.parse(JSON.stringify(nonNullRaw));
+		Object.keys(schema).forEach((key) => delete schema[key]);
+		Object.assign(schema, nonNull);
+		schema.nullable = true;
+		preprocess(schema);
+	} else if (Object.keys(schema).length === 1 && schema["$ref"]) {
+		schema.table = true;
+		schema.deepness = 0;
+	} else {
+		console.error("Impossible case reached", schema);
+		return;
 	}
-	schema.nullable = nullable;
-	schema.deepness = deepness;
-	schema.table = deepness >= 1 && Object.keys(schema.properties).length > THRESHOLD;
+
+	delete schema.required;
 }
 
+function collapse(schema) {
+	function collapseRecursive(obj, path, propagate) {
+		let result = {};
+
+		Object.entries(obj.properties || {}).forEach(([key, val]) => {
+			if (val.type === "object" && val.properties && propagate && !val.table) {
+				let nested = collapseRecursive(val, path.concat(key), true);
+				Object.entries(nested).forEach(([nestedKey, nestedVal]) => {
+					result[`${key}_${nestedKey}`] = nestedVal;
+				});
+			} else if (val.type === "array" && val.items && val.items.type === "object" && val.items.properties) {
+				collapseRecursive(val.items, path.concat(key), false);
+				result[key] = val;
+			} else if (val.type === "object" && val.properties && val.table) {
+				collapseRecursive(val, path.concat(key), false);
+				result[key] = val;
+			} else {
+				result[key] = val;
+			}
+		});
+
+		obj.properties = result;
+		return result;
+	}
+
+	collapseRecursive(schema, [], true);
+}
+
+function strip(schema) {
+	const stop = schema.deepness === 0;
+	delete schema.deepness;
+	if (stop) return;
+	for (const val of Object.values(schema)) {
+		if (typeof val === "object" && val !== null) strip(val);
+	}
+}
+
+function generateTables(tables, name, schema) {}
+
 function convertSchema(root) {
+	const tables = {};
 	Object.entries(root.definitions).forEach(([name, schema]) => {
-		help(schema);
+		// console.log(JSON.stringify(schema, null, 4));
+		preprocess(schema);
+		collapse(schema);
+		strip(schema);
+		generateTables(tables, name, schema);
 	});
 }
 
 function JSONtoDB(interfacesFolderPath, name = null) {
 	try {
 		execSync(
-			`typescript-json-schema --strictNullChecks true --defaultNumberType "integer" --out schema.json ${interfacesFolderPath}/*.ts  *`,
+			`typescript-json-schema --strictNullChecks true --required true --defaultNumberType "integer" --out schema.json ${interfacesFolderPath}/*.ts  *`,
 			{ stdio: "inherit" }
 		);
 	} catch (error) {
@@ -71,7 +153,7 @@ function JSONtoDB(interfacesFolderPath, name = null) {
 	}
 
 	const schema = JSON.parse(fs.readFileSync("schema.json", "utf-8"));
-	const { columns, deepness } = convertSchema(schema);
+	convertSchema(schema);
 	return columns;
 }
 
--- interfaces/user.ts
+++ interfaces/user.ts
@@ -1,44 +1,42 @@
 interface User {
-    avatar_url: string;
-    country: { code: string; name: string };
-    country_code: string;
-    cover: { custom_url: null | string; id: null | number; url: string };
-    default_group: string;
-    groups: {
-        colour: null | string;
-        has_listing: boolean;
-        has_playmodes: boolean;
-        id: number;
-        identifier: string;
-        is_probationary: boolean;
-        name: string;
-        playmodes: null | ("osu" | "taiko" | "fruits" | "mania")[];
-        short_name: string;
-    }[];
-    id: number;
-    is_active: boolean;
-    is_bot: boolean;
-    is_deleted: boolean;
-    is_online: boolean;
-    is_supporter: boolean;
-    last_visit: null
-    | Date;
-    pm_friends_only: boolean;
-    profile_colour: null | string;
-    statistics_rulesets: {
-        fruits?: Statistics;
-        mania?: Statistics;
-        osu?: Statistics;
-        taiko?: Statistics;
-    };
-    team: | null
-    | { flag_url: string; id: number; name: string; short_name: string };
-    username: string;
-    variants?: {
-        country_rank: null | number;
-        global_rank: null | number;
-        mode: "osu" | "taiko" | "fruits" | "mania";
-        pp: number;
-        variant: string;
-    }[];
+	avatar_url: string;
+	country: { code: string; name: string };
+	country_code: string;
+	cover: { custom_url: null | string; id: null | number; url: string };
+	default_group: string;
+	groups: {
+		colour: null | string;
+		has_listing: boolean;
+		has_playmodes: boolean;
+		id: number;
+		identifier: string;
+		is_probationary: boolean;
+		name: string;
+		playmodes: null | ("osu" | "taiko" | "fruits" | "mania")[];
+		short_name: string;
+	}[];
+	id: number;
+	is_active: boolean;
+	is_bot: boolean;
+	is_deleted: boolean;
+	is_online: boolean;
+	is_supporter: boolean;
+	last_visit: null | Date;
+	pm_friends_only: boolean;
+	profile_colour: null | string;
+	statistics_rulesets: {
+		fruits?: Statistics;
+		mania?: Statistics;
+		osu?: Statistics;
+		taiko?: Statistics;
+	};
+	team: null | { flag_url: string; id: number; name: string; short_name: string };
+	username: string;
+	variants?: {
+		country_rank: null | number;
+		global_rank: null | number;
+		mode: "osu" | "taiko" | "fruits" | "mania";
+		pp: number;
+		variant: string;
+	}[];
 }
